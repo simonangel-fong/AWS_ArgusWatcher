6077da3713014889759616970c9cd892b771806a
simonangel-fong <simonangelfong@gmail.com>
2023-09-14 01:49:36 -0400

    test: codedeploy07

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -y install python3-venv # Install pip package
-echo "install env" > ~/log
+# sudo apt-get -y install python3-venv # Install pip package
+# echo "install env" > ~/log
 sudo rm -rf ~/env     # remove existing venv
 echo "remove env" > ~/log
 python3 -m venv env # Creates virtual environment
