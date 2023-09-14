c92db6df47e9bb110a1ee3d98142cb84fb33c24a
simonangel-fong <simonangelfong@gmail.com>
2023-09-14 02:48:00 -0400

    test: static file03

--- ArgusWatcher/settings.py
+++ ArgusWatcher/settings.py
@@ -108,7 +108,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
