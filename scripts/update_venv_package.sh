3a039e5affeed71ea8024528d5c33e537e98a49f
simonangel-fong <simonangelfong@gmail.com>
2023-09-14 02:01:22 -0400

    test: codedeploy09

--- scripts/update_venv_package.sh
+++ scripts/update_venv_package.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 source ~/env/bin/activate # activate venv
+echo "activate" >> ~/log
 pip install -r /home/ubuntu/ArgusWatcher/requirements.txt
+echo "requirements" >> ~/log
 deactivate
